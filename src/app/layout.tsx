c5dd077797ce63b61570fdb594882d91d8ebec5e
AndrewWang <andrewwang123118@gmail.com>
2025-06-26 18:48:46 -0500

    save

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,6 +36,26 @@ export const metadata: Metadata = {
       { rel: 'apple-touch-icon-precomposed', url: '/images/logo.png' }
     ]
   },
+  openGraph: {
+    type: "website",
+    siteName: "GPA Calculator",
+    title: undefined,
+    description: "GPA Calculator for Allen ISD!",
+    images: [
+      {
+        url: "/images/logo.png",
+        width: 800,
+        height: 800,
+        alt: "GPA Calculator Logo",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: undefined,
+    description: "GPA Calculator for Allen ISD!",
+    images: ["/images/logo.png"],
+  },
 };
 
 export default function RootLayout({
@@ -46,22 +66,6 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        {/* meta tags for opengraph and twtiter */}
-        <meta property="og:site_name" content="GPA Calculator" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="AISD GPA Calculator" />
-        <meta property="og:description" content="GPA Calculator for Allen ISD!" />
-        <meta property="og:url" content="https://gpa-calculator-andrxwwxng.vercel.app" />
-        <meta property="og:image" content="https://gpa-calculator-andrxwwxng.vercel.app/images/gpalogo.png" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-        <meta property="og:image:type" content="image/png" />
-
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="AISD GPA Calculator" />
-        <meta name="twitter:description" content="GPA Calculator for Allen ISD!" />
-        <meta name="twitter:image" content="https://gpa-calculator-andrxwwxng.vercel.app/images/gpalogo.png" />
-
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-5MBD543C26"
           strategy="afterInteractive"
